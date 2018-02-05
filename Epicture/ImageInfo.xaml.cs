b44339cb7f7c2729dbda33a68b34019a37db1536
Marc-Antoine Leconte <marc-antoine.leconte@epitech.eu>
2018-02-05 17:45:05 +0100

    Boostrap(init): set the basis of the project

---
+++ Epicture/ImageInfo.xaml.cs
@@ -0,0 +1,41 @@
+﻿using FlickrNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Epicture
+{
+    /// <summary>
+    /// Interaction logic for ImageInfo.xaml
+    /// </summary>
+    public partial class ImageInfo : UserControl
+    {
+        public Photo photo;
+
+        public ImageInfo(Photo photo_)
+        {
+            InitializeComponent();
+            photo = photo_;
+            LoadImage();
+        }
+
+        private void LoadImage()
+        {
+            BitmapImage bitmap = new BitmapImage();
+            bitmap.BeginInit();
+            bitmap.UriSource = new Uri(photo.Small320Url, UriKind.Absolute);
+            bitmap.EndInit();
+
+            ImageSource imageSource = bitmap;
+            Image.Source = imageSource;
+            Title.Text = photo.Title;
+        }
+    }
+}
---
+++ Epicture/Managers.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Epicture
+{
+    class Managers
+    {
+        private static Managers _instance;
+
+        public static Managers Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Managers();
+                }
+                return _instance;
+            }
+        }
+
+        public FlickerManager flicker;
+
+        private Managers()
+        {
+            flicker = new FlickerManager();
+        }
+
+    }
+}
