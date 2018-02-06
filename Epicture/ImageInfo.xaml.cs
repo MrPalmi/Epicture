2dfab21c7cf128c519ce37cb0dd16a26585fd79b
Marc-Antoine Leconte <marc-antoine.leconte@epitech.eu>
2018-02-06 21:04:52 +0100

    Merge branch 'master' into DownloadImageAndDefaultSearch

--- Epicture/ImageInfo.xaml.cs
+++ Epicture/ImageInfo.xaml.cs
@@ -41,6 +41,37 @@ namespace Epicture
             ImageSource imageSource = bitmap;
             Image.Source = imageSource;
             Title.Text = photo.Title;
+
+            if (Managers.Instance.user.Connected)
+            {
+                var photos = Managers.Instance.flicker.flickr.FavoritesGetList(Managers.Instance.flicker.accessToken.UserId);
+
+                foreach (var it in photos)
+                {
+                    if (it.PhotoId == photo.PhotoId)
+                    {
+                        StarsIcon.Foreground = new SolidColorBrush(Colors.Gray);
+                        return;
+                    }
+                }
+            }
+            else
+                Stars.Visibility = Visibility.Collapsed;
+        }
+
+        private void Favorite(object sender, RoutedEventArgs e)
+        {
+            var photos = Managers.Instance.flicker.flickr.FavoritesGetList(Managers.Instance.flicker.accessToken.UserId);
+
+            foreach (var it in photos)
+            {
+                if (it.PhotoId == photo.PhotoId)
+                {
+                    Managers.Instance.flicker.UnsetFavorite(it.PhotoId);
+                    return;
+                }
+            }
+            Managers.Instance.flicker.SetFavorite(photo.PhotoId);
         }
 
 		private void DownloadImage(object sender, RoutedEventArgs e)
